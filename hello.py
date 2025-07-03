57e10d9e7949d41130abde4521306eaf9a87f648
macabhy <abhaykumar754@gmail.com>
2025-07-03 17:45:19 +0530

    Update hello.py with repo scanning only

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # Triggering GitHub Actions for ScanCode
 # Simple test file to trigger license scan
 # Running corrected ScanCode workflow
+# Scan only repo python file
 print("Hello from FOSS scan demo!")
